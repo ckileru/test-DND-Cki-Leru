421dd80ae8e2b8de4b76796262abc10ac1cb6644
violetorviolent <128536179+violetorviolent@users.noreply.github.com>
2023-07-12 08:10:36 -0600

    Add files via upload
    Second installment - polished Hero-section, mockup of dress-up and race-select

---
+++ app.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', e => {
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+if (ScrollTrigger.isTouch !== 1) {
+
+	ScrollSmoother.create({
+		wrapper: '.wrapper',
+		content: '.content',
+		smooth: 1.5,
+		effects: true
+	})
+}
--- main.js
+++ main.js
@@ -1,53 +1,132 @@
 const navBtn = document.querySelector('#menu');
-const menuBar = document.querySelector('[role="menubar"]');
+const menuBar = document.querySelector('#menubar');
+const iconClose = document.querySelector('.icon-close');
+const iconOpen = document.querySelector('.icon-open');
 
-navBtn.addEventListener('click', () =>{
-    menuBar.classList.toggle("hidden");
-    menuBar.classList.toggle("flex");
+navBtn.addEventListener('click', () => {
+  menuBar.classList.toggle("hidden");
+  iconClose.classList.toggle("hidden");
+  iconOpen.classList.toggle("hidden");
 });
 
-const menuItem = document.querySelector('[role="menuitem"]');
-
-menuItem.addEventListener('click', () =>{
-    menuBar.classList.remove("flex");
-    menuBar.classList.add("flex");
+document.addEventListener('click', (event) => {
+  if (!menuBar.contains(event.target) && !navBtn.contains(event.target)) {
+    menuBar.classList.add('hidden');
+    iconOpen.classList.remove("hidden");
+    iconClose.classList.add("hidden");
+  }
+});
+document.addEventListener('touch', (event) => {
+  if (!menuBar.contains(event.target) && !navBtn.contains(event.target)) {
+    menuBar.classList.add('hidden');
+    iconOpen.classList.remove("hidden");
+    iconClose.classList.add("hidden");
+  }
 });
 
-window.addEventListener('scroll', e => {
-	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
-})
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
-if (ScrollTrigger.isTouch !== 1) {
 
-	ScrollSmoother.create({
-		wrapper: '.wrapper',
-		content: '.content',
-		smooth: 1.5,
-		effects: true
-	})
 
-	gsap.fromTo('.hero-section', { opacity: 1 }, {
-		opacity: 0,
-		scrollTrigger: {
-			trigger: '.hero-section',
-			start: 'center',
-			end: '820',
-			scrub: true
-		}
-	})
+/*********** DRESS UP */
+let state = {
+  dress: 0,
+  hair: 0,
+  hat: 0,
+  shoes: 0,
+  accessory: 0,
+  face: 0,
+};
+nextdress();
+nexthair();
+nextface();
 
-	let itemsL = gsap.utils.toArray('.gallery__item')
-
-	itemsL.forEach(item => {
-		gsap.fromTo(item, { opacity: 0 , y: 100}, {
-			opacity: 1, y: 0,
-			scrollTrigger: {
-				trigger: item,
-				start: '-500',
-				end: '-100',
-				scrub: true
-			}
-		})
-	})
+function nextdress() {
+  let dress = document.querySelector("#dress");
+  if (state.dress < 7) {
+    state.dress++;
+    dress.setAttribute("class", `dress${state.dress}`);
+  } else if (state.dress === 7) {
+    state.dress = 0;
+    dress.setAttribute("class", `dress${state.dress}`);
+  }
 }
+
+
+
+/*************SCROLL BAR TABS */
+const tabs = document.querySelectorAll(".scrollable-tabs-container a");
+const rightArrow = document.querySelector(
+  ".scrollable-tabs-container .right-arrow svg"
+);
+const leftArrow = document.querySelector(
+  ".scrollable-tabs-container .left-arrow svg"
+);
+const tabsList = document.querySelector(".scrollable-tabs-container ul");
+const leftArrowContainer = document.querySelector(
+  ".scrollable-tabs-container .left-arrow"
+);
+const rightArrowContainer = document.querySelector(
+  ".scrollable-tabs-container .right-arrow"
+);
+
+const removeAllActiveClasses = () => {
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+};
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    removeAllActiveClasses();
+    tab.classList.add("active");
+  });
+});
+
+const manageIcons = () => {
+  if (tabsList.scrollLeft >= 20) {
+    leftArrowContainer.classList.add("active");
+  } else {
+    leftArrowContainer.classList.remove("active");
+  }
+
+  let maxScrollValue = tabsList.scrollWidth - tabsList.clientWidth - 20;
+  console.log("scroll width: ", tabsList.scrollWidth);
+  console.log("client width: ", tabsList.clientWidth);
+
+  if (tabsList.scrollLeft >= maxScrollValue) {
+    rightArrowContainer.classList.remove("active");
+  } else {
+    rightArrowContainer.classList.add("active");
+  }
+};
+
+rightArrow.addEventListener("click", () => {
+  tabsList.scrollLeft += 200;
+  manageIcons();
+});
+
+leftArrow.addEventListener("click", () => {
+  tabsList.scrollLeft -= 200;
+  manageIcons();
+});
+
+tabsList.addEventListener("scroll", manageIcons);
+
+let dragging = false;
+
+const drag = (e) => {
+  if (!dragging) return;
+  tabsList.classList.add("dragging");
+  tabsList.scrollLeft -= e.movementX;
+};
+
+tabsList.addEventListener("mousedown", () => {
+  dragging = true;
+});
+
+tabsList.addEventListener("mousemove", drag);
+
+document.addEventListener("mouseup", () => {
+  dragging = false;
+  tabsList.classList.remove("dragging");
+});
