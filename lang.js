25d7b9683aff373825048782726844cc2f81cb3b
violetorviolent <128536179+violetorviolent@users.noreply.github.com>
2023-11-19 05:14:53 -0600

    update at 19.11.23
    still gotta write popup images and fix inner navigation which bugs for seemingly no reason

---
+++ lang.js
@@ -0,0 +1,27 @@
+const langArr ={
+    'site-name' :{
+        'ru': 'Приветствуем тебя, о Странник!',
+        'en': 'Приветствуем тебя, о Странник!',
+        'sp': 'Приветствуем тебя, о Странник!'
+    },
+    'title-1' :{
+        'ru': 'name1 in rus',
+        'en': 'name1 in en',
+        'sp': 'name1 in sp'
+    },
+    'text-1' :{
+        'ru': 'text1 in rus',
+        'en': 'text1 in en',
+        'sp': 'text1 in sp'
+    },
+    'title-2' :{
+        'ru': 'name2 in rus',
+        'en': 'name2 in eng',
+        'sp': 'name2 in sp'
+    },
+    'text-2' :{
+        'ru': 'text2 in rus',
+        'en': 'text2 in eng',
+        'sp': 'text2 in sp'
+    }
+}
--- main.js
+++ main.js
@@ -37,3 +37,33 @@ document.addEventListener('touch', (event) => {
     iconClose.classList.add("hidden");
   }
 });
+
+/** Language switch */
+const allLang = ['ru', 'en', 'sp'];
+let select =document.querySelector('select');
+
+function changeURLLanguage(){
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+select.addEventListener('change', changeURLLanguage);
+
+function changeLanguage(){
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    if(!allLang.includes(hash)){
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    select.value = hash;
+    for(let key in langArr){
+        let elem = document.querySelector('.lng-'+key);
+        if(elem){
+            elem.innerHTML =langArr[key][hash];
+        };
+    };
+}
+changeLanguage()
+
